fbe1b16b78a6ef92965db7fa247504aebfc7ab6b
MAHMOUD-X-EMAD <mahmoudemadmahmoud1213@gmail.com>
2023-05-24 23:45:59 +0300

    Login And Registeration Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './Modules/home/home.module';
-import { AuthModule } from './Modules/auth/auth/auth.module';
 import { UserModule } from './Modules/user/user.module';
 import { DoctorModule } from './Modules/doctor/doctor.module';
 import { SharedModule } from './Modules/shared/shared.module';
+import { LoginComponent } from './Modules/auth/login/login.component';
+import { RegisterComponent } from './Modules/auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HomeModule,
-    AuthModule,
+    
     UserModule,
     DoctorModule,
     SharedModule
